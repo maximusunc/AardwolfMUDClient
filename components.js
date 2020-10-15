df3c06fb47eb448c01fdd30cb8b3e2380adbd4bc
Max Wang <max@covar.com>
2020-10-15 18:01:23 -0400

    This is what should be v1.0.0

---
+++ components.js
@@ -0,0 +1,170 @@
+// blessed is for all the gui
+const blessed = require('neo-blessed');
+
+// Initialize a blessed screen
+const screen = blessed.screen({
+  smartCSR: true,
+  cursor: {
+    blink: true,
+    shape: 'block',
+  },
+  title: 'Aardwolf!',
+});
+
+const map = blessed.box({
+  parent: screen,
+  width: '25%',
+  height: '50%',
+  left: '35%',
+  top: 0,
+  border: 'line',
+  focusable: false,
+  mouse: false,
+});
+
+const filler = blessed.box({
+  parent: screen,
+  width: '65%',
+  height: '27%',
+  left: '35%',
+  bottom: 17,
+  border: 'line',
+  focusable: false,
+  mouse: false,
+});
+const filler_2 = blessed.box({
+  parent: screen,
+  width: '40%',
+  height: '50%',
+  left: '60%',
+  border: 'line',
+  focusable: false,
+  mouse: false,
+})
+
+const stats_box = blessed.box({
+  parent: screen,
+  width: '65%',
+  height: '25%',
+  left: '35%',
+  bottom: 0,
+  border: 'line',
+  focusable: false,
+  mouse: false,
+});
+
+const bar_settings = {
+  parent: stats_box,
+  width: '85%',
+  height: 1,
+  left: 0,
+  pch: '#'
+};
+
+const label_settings = {
+  parent: stats_box,
+  height: 1,
+  left: '86%',
+  align: 'center',
+  content: '',
+};
+
+const blessing = blessed.text({
+  parent: filler,
+  top: 0,
+  left: 0,
+  content: '',
+});
+
+const health = blessed.progressbar({
+  bottom: 9,
+  barFg: 'red',
+  ...bar_settings,
+});
+const health_label = blessed.text({
+  bottom: 9,
+  ...label_settings,
+});
+const mana = blessed.progressbar({
+  bottom: 6,
+  barFg: 'green',
+  ...bar_settings,
+});
+const mana_label = blessed.text({
+  bottom: 6,
+  ...label_settings,
+});
+const moves = blessed.progressbar({
+  bottom: 3,
+  barFg: 'blue',
+  ...bar_settings,
+});
+const moves_label = blessed.text({
+  bottom: 3,
+  ...label_settings,
+});
+const badguy = blessed.progressbar({
+  bottom: 0,
+  barFg: 'yellow',
+  ...bar_settings,
+});
+const badguy_label = blessed.text({
+  bottom: 0,
+  ...label_settings,
+});
+
+const next_level = blessed.text({
+  parent: stats_box,
+  top: 0,
+  left: 0,
+  content: '',
+});
+const quest_time = blessed.text({
+  parent: stats_box,
+  top: 2,
+  left: 0,
+  content: '',
+});
+
+const output = blessed.log({
+  parent: screen,
+  width: '35%',
+  height: '100%-3',
+  left: 0,
+  top: 0,
+  border: 'line',
+  mouse: true,
+  scrollable: true,
+  alwaysScroll: true,
+  scrollback: 1000,
+  scrollbar: {
+    style: {
+      bg: 'yellow',
+    },
+  },
+  content: '',
+});
+const input = blessed.textbox({
+  parent: screen,
+  cursor: 'block',
+  cursorBlink: true,
+  width: '35%',
+  height: 3,
+  left: 0,
+  bottom: 0,
+  border: 'line',
+  focused: true,
+  inputOnFocus: true,
+  scrollable: false,
+  mouse: false,
+});
+
+module.exports = {
+  screen, input, output,
+  health, health_label,
+  mana, mana_label,
+  moves, moves_label,
+  badguy, badguy_label,
+  next_level, quest_time,
+  blessing,
+};
--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const net = require('net');
+const fs = require('fs');
 // telnet-stream basically strips out all the telnet config messages.
 const { TelnetSocket } = require('telnet-stream');
-// blessed is for all the gui
-const blessed = require('neo-blessed');
+
+const {
+  screen, input, output,
+  health, health_label,
+  mana, mana_label,
+  moves, moves_label,
+  badguy, badguy_label,
+  next_level, quest_time,
+  blessing,
+} = require('./components');
+
+if (!fs.existsSync('logs')) {
+  fs.mkdirSync('logs');
+}
+
+const now = new Date();
+// this is stupid, but it can't be inline
+const day = now.getDate();
+const timestamp = `${now.getFullYear()}${now.getMonth() + 1}${day}${now.getHours()}${now.getMinutes()}${now.getSeconds()}`;
+const writeStream = fs.createWriteStream(`logs/${timestamp}.log`);
 
 // create the telnet connection
 const socket = net.createConnection(23, '23.111.136.202');
@@ -12,79 +31,27 @@ const tsock = new TelnetSocket(socket);
 
 const history = [];
 let historyInd = 0;
+let loggedIn = false;
 
-// Initialize a blessed screen
-const screen = blessed.screen({
-  smartCSR: true,
-  cursor: {
-    blink: true,
-    shape: 'block',
-  },
-  title: 'Aardwolf!',
+input.on('submit', (msg) => {
+  if (msg && loggedIn) {
+    historyInd = history.push(msg);
+  }
+  input.clearValue();
+  input.focus();
+  tsock.write(msg + '\n');
+  if (input.secret) {
+    loggedIn = true;
+  }
+  screen.render();
 });
-
-const map = blessed.box({
-  parent: screen,
-  width: '25%',
-  height: '50%',
-  right: 25,
-  top: 0,
-  border: 'line',
-  focusable: false,
-  mouse: false,
-});
-const stats = null;
-const output = blessed.log({
-  parent: screen,
-  width: '50%',
-  height: '100%-4',
-  left: 0,
-  top: 0,
-  border: {
-    type: 'line',
-  },
-  mouse: true,
-  scrollable: true,
-  alwaysScroll: true,
-  scrollbar: {
-    style: {
-      bg: 'yellow',
-    },
-  },
-  content: '',
-});
-const input = blessed.textbox({
-  parent: screen,
-  cursor: 'block',
-  cursorBlink: true,
-  width: '50%',
-  height: 4,
-  left: 0,
-  bottom: 0,
-  border: 'line',
-  focused: true,
-  inputOnFocus: true,
-  scrollable: false,
-  mouse: false,
-});
-
-// input.on('submit', (msg) => {
-//   // output.log(msg);
-//   if (msg) {
-//     historyInd = history.push(msg);
-//   }
-//   input.clearValue();
-//   input.readInput(() => {});
-//   screen.render();
-//   return tsock.write(msg);
-// });
 input.key('up', () => {
   // this will stop at 0 or null
   if (historyInd) {
     historyInd -= 1;
     input.setValue(history[historyInd]);
+    screen.render();
   }
-  screen.render();
 });
 input.key('down', () => {
   // stop if we're at the end
@@ -92,64 +59,75 @@ input.key('down', () => {
     historyInd += 1;
     input.setValue(history[historyInd]);
   } else {
+    historyInd = history.length;
     input.setValue('');
   }
   screen.render();
 });
 input.key('C-c', () => {
-  tsock.write('quit');
-  screen.render();
+  tsock.write('quit\n');
   process.exit();
 });
 
 function ingest(message) {
-  const lines = message.split('\r\r');
+  if (message.includes('Password: ')) {
+    input.secret = true;
+  } else {
+    input.secret = false;
+  }
+  writeStream.write(String.raw`${message}`);
+  const lines = message.split('\n');
   lines.forEach((line) => {
     const hasMap = RegExp(/[a-zA-Z0-9]+/g);
+    // "[*Daily Blessing*] [678/678hp 666/666mn 993/993mv 0qt 746tnl] >"
+    // "[Fighting: 587/717hp 700/700mn 1025/1025mv 695tnl Enemy: 45% ]>"
+    const playerStats = RegExp(/(?<prefix>[\[A-Za-z: ]*)(?<hp>[0-9]+)\/(?<totalhp>[0-9]+)hp (?<mn>[0-9]+)\/(?<totalmn>[0-9]+)mn (?<mv>[0-9]+)\/(?<totalmv>[0-9]+)mv (?<qt>[0-9]+)qt? (?<tnl>[0-9]+)tnl/g);
+    const enemyStats = RegExp(/ Enemy: (?<enemy>[0-9]+)%/g);
+    const dailyBlessing = RegExp(/\[.*\*.*Daily Blessing.*\*.*]/);
 
-    // if (RegExp("(?P<prefix>[\[A-Za-z: ]*)(?P<hp>[0-9]+)/(?P<totalhp>[0-9]+)hp (?P<mn>[0-9]+)/(?P<totalmn>[0-9]+)mn (?P<mv>[0-9]+)/(?P<totalmv>[0-9]+)mv( [0-9]+qt)? (?P<tnl>[0-9]+)tnl").test(message)) {
-    //   output.pushLine('we have player stats');
-    // }
-    if (!hasMap.test(line)) {
-      map.setContent(line);
+    const stats = [...line.matchAll(playerStats)];
+    const enStats = [...line.matchAll(enemyStats)];
+    const haveBlessing = dailyBlessing.test(line);
+    if (haveBlessing) {
+      blessing.setContent('Ayla wants to bless you.');
     } else {
-      output.log(line);
+      blessing.setContent('');
+    }
+    if (stats.length) {
+      const { hp, totalhp, mn, totalmn, mv, totalmv, qt, tnl } = stats[0].groups;
+      health.setProgress(hp / totalhp * 100);
+      mana.setProgress(mn / totalmn * 100);
+      moves.setProgress(mv / totalmv * 100);
+      health_label.setContent(`HP ${hp}/${totalhp}`);
+      mana_label.setContent(`MN ${mn}/${totalmn}`);
+      moves_label.setContent(`MV ${mv}/${totalmv}`);
+      next_level.setContent(`${tnl} experience points until you level up!`);
+      quest_time.setContent(qt !== '0' ? `${qt} minutes until next quest` : 'Go quest!');
+      badguy.setProgress(0);
+      badguy_label.setContent('');
+    } else {
+      output.log(line.replace(/\r/g, ''));
+    }
+    if (enStats.length) {
+      const { enemy } = enStats[0].groups;
+      badguy.setProgress(enemy);
+      badguy_label.setContent(`Baddie at ${enemy}%`);
     }
   });
-  screen.render();
 }
 
 // exit the process when the telnet connection is closed
 tsock.on('close', () => {
+  writeStream.end();
   return process.exit();
 });
 
 // listen to all incoming messages
 tsock.on('data', (chunk) => {
   const msg = chunk.toString('utf8');
-  if (msg.includes('Password: ')) {
-    input.censor = true;
-    screen.render();
-  } else {
-    input.censor = false;
-  }
   // display the message from telnet
   ingest(msg);
   // console.log(out.replace(/\r/g, '**newline**'));
-});
-
-process.stdin.setRawMode(false);
-process.stdin.resume();
-
-process.stdin.on('data', (buf) => {
-  const ipt = buf.toString('utf8');
-  if (ipt) {
-    historyInd = history.push(ipt);
-  }
-  input.clearValue();
-  input.focus();
-  screen.render();
-  return tsock.write(ipt);
 });
 
 input.readInput(() => {});
