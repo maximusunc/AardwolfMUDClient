771f7b0648460e3f069a77dacf1eb9dba66fb377
Max Wang <max@covar.com>
2020-10-22 20:07:34 -0400

    Let X in the map

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function ingest(message) {
     const colorChars = RegExp(/\[\d;\d{2}m/g);
     const noColorLine = line.replace(colorChars, '');
     writeStream.write(noColorLine);
-    const hasMap = RegExp(/^[^a-zA-Z]+$/g);
+    const hasMap = RegExp(/^[^a-zA-CE-WY-Z]+ +$/g);
     // "[*Daily Blessing*] [678/678hp 666/666mn 993/993mv 0qt 746tnl] >"
     // "[Fighting: 587/717hp 700/700mn 1025/1025mv 695tnl Enemy: 45% ]>"
     const playerStats = RegExp(/(?<prefix>[\[A-Za-z: ]*)(?<hp>[0-9]+)\/(?<totalhp>[0-9]+)hp (?<mn>[0-9]+)\/(?<totalmn>[0-9]+)mn (?<mv>[0-9]+)\/(?<totalmv>[0-9]+)mv((?<qt> [0-9]+)qt)? (?<tnl>[0-9]+)tnl/g);
