9f44e72b13df8f130eb8a7546dae6874b4decdc1
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-02-06 22:01:03 -0500

    Shuffle code around a bit

--- src/output.js
+++ src/output.js
@@ -15,53 +15,66 @@ let gettingMap = false;
 let gettingInv = false;
 let inv = '';
 
-function parseMessage(self, msg) {
+function extractGroup(self, msg) {
   let groupStats = msg.match(/(-+.*\[.*Group:.*\].*-+)(.*)(\d{1,}\/\d{1,} +?\d{1,}. +?\d{1,}.*?\d{1,}.*  Y?)/s);
-  if (groupStats) {
-    groupStats = groupStats[0];
-    const group = ansiup.ansi_to_html(groupStats);
-    self.group = group;
-    msg = msg.replace(groupStats, '');
+  if (!groupStats) {
+    return msg
   }
+  groupStats = groupStats[0];
+  const group = ansiup.ansi_to_html(groupStats);
+  self.group = group;
+  return msg.replace(groupStats, '');
+}
 
-  const stats = {};
+function extractMap(self, msg) {
   // Grab map. If it exists, put it in the map box
   const mapStart = msg.match(/(?<=<MAPSTART>)(.*)/s);
-  if (mapStart || gettingMap) {
-    gettingMap = true;
-    mapContents += mapStart ? mapStart[0] : msg;
-    if (mapContents.indexOf('<MAPEND>') > -1) {
-      mapContents = mapContents.split('<MAPEND>')[0];
-      mapContents = ansiup.ansi_to_html(mapContents);
-      self.map = mapContents;
-      mapContents = '';
-      gettingMap = false;
-    }
-    // remove from output log
-    // This will only replace maps that are in the same message
-    msg = msg.replace(/<MAPSTART>(.*)<MAPEND>/s, '');
+  if (!mapStart && !gettingMap) {
+    return msg
   }
+  gettingMap = true;
+  mapContents += mapStart ? mapStart[0] : msg;
+  if (mapContents.indexOf('<MAPEND>') > -1) {
+    mapContents = mapContents.split('<MAPEND>')[0];
+    mapContents = ansiup.ansi_to_html(mapContents);
+    self.map = mapContents;
+    mapContents = '';
+    gettingMap = false;
+  }
+  // remove from output log
+  // This will only replace maps that are in the same message
+  return msg.replace(/<MAPSTART>(.*)<MAPEND>/s, '');
+}
 
+function extractInventory(self, msg) {
   // Grab inventory. It will likely come as multiple messages, so we need to keep track
   const invStart = msg.match(/(?<={inventory})(.*)/s);
-  if (invStart || gettingInv) {
-    gettingInv = true;
-    inv += invStart ? invStart[0] : msg;
-    // if the inventory has closing tag
-    if (inv.indexOf('{/inventory}') > -1) {
-      // grab everything before closing tag
-      inv = inv.split('{/inventory}')[0];
-      // display inventory
-      inv = ansiup.ansi_to_html(inv);
-      self.inventory = inv;
-      // clear inventory for next time
-      inv = '';
-      gettingInv = false;
-    }
-    // remove from output log
-    msg = msg.replace(/({inventory})?(.*)({\/inventory})?/s, '');
+  if (!invStart && !gettingInv) {
+    return msg
   }
+  gettingInv = true;
+  inv += invStart ? invStart[0] : msg;
+  // if the inventory has closing tag
+  if (inv.indexOf('{/inventory}') > -1) {
+    // grab everything before closing tag
+    inv = inv.split('{/inventory}')[0];
+    // display inventory
+    inv = ansiup.ansi_to_html(inv);
+    self.inventory = inv;
+    // clear inventory for next time
+    inv = '';
+    gettingInv = false;
+  }
+  // remove from output log
+  return msg.replace(/({inventory})?(.*)({\/inventory})?/s, '');
+}
 
+function parseMessage(self, msg) {
+  msg = extractGroup(self, msg);
+  msg = extractMap(self, msg);
+  msg = extractInventory(self, msg);
+
+  const stats = {};
   // "[*Daily Blessing*] [678/678hp 666/666mn 993/993mv 0qt 746tnl] >"
   // const playerStats = RegExp(/(?<prefix>\[[A-Za-z: ]*)(?<hp>[0-9]+)\/(?<totalhp>[0-9]+)hp (?<mn>[0-9]+)\/(?<totalmn>[0-9]+)mn (?<mv>[0-9]+)\/(?<totalmv>[0-9]+)mv((?<qt> [0-9]+)qt)? (?<tnl>[0-9]+)tnl.+?>/g);
   const playerStats = RegExp(/(?<prefix>\[[^\][A-Za-z: ].*?)(?<hp>[0-9]+)\/(?<totalhp>[0-9]+)hp (?<mn>[0-9]+)\/(?<totalmn>[0-9]+)mn (?<mv>[0-9]+)\/(?<totalmv>[0-9]+)mv((?<qt> [0-9]+)qt)? (?<tnl>[0-9]+)tnl.+?>/g);
