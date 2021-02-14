c113b925669648f2917b2716e55dce4d8b606d4d
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-02-14 14:47:59 -0500

    Overhaul inventory handling

--- src/Inventory.svelte
+++ src/Inventory.svelte
@@ -1,20 +1,50 @@
 <script>
-    import { output } from './output';
+    import { captor, output, strip_colors } from './output';
+    const { ipcRenderer } = require('electron');
+    const handleRefresh = () => {
+        Object.keys($output.containers).forEach((containerid) => {
+            captor.askForContainer(containerid);
+        })
+    }
+    function openTab(tabName) {
+        // show chosen panel
+        let panels = document.getElementsByClassName("panel")
+        Array.from(panels).forEach(panel => {
+            panel.style.display = "none";
+        })
+        document.getElementById(`${tabName}_panel`).style.display = "block";
+
+        // highlight chosen tab
+        let tabs = document.getElementsByClassName("tab")
+        Array.from(tabs).forEach(tab => {
+            tab.classList.remove("mm_active");
+        })
+        document.getElementById(`${tabName}_tab`).classList.add("mm_active");
+    }
 </script>
 
-<style>
-    #inventory {
-        grid-column: 2 / span 3;
-        grid-row: 2;
-        height: 100%;
-        overflow-y: auto;
-    }
-    pre {
-        margin: 0;
-        white-space: pre-wrap;
-    }
-</style>
+<link rel="stylesheet" href="https://unpkg.com/purecss@2.0.5/build/pure-min.css" integrity="sha384-LTIDeidl25h2dPxrB2Ekgc9c7sEC3CWGM6HeFmuDNUjX76Ert4Z4IY714dhZHPLd" crossorigin="anonymous">
 
 <div id="inventory">
-    <pre>{@html $output.inventory}</pre>
+    <div class="pure-menu pure-menu-horizontal">
+        <ul class="pure-menu-list">
+            {#each [...Object.keys($output.containers)].reverse() as containerid}
+                <li class="pure-menu-item tab" id="{containerid}_tab" >
+                    <a class="pure-menu-link" on:click={() => {openTab(containerid)}}>{@html containerid.match(/[a-z]/i) ? containerid : strip_colors($output.items.get(containerid).itemname)}</a>
+                </li>
+            {/each}
+            <!-- <li class="pure-menu-item">
+                <a class="pure-menu-link tab" id="refresh_tab" on:click={handleRefresh}>refresh</a>
+            </li> -->
+        </ul>
+    </div>
+    {#each [...Object.keys($output.containers)].reverse() as containerid}
+        <div id="{containerid}_panel" class="panel" style="display: none">
+            <ul>
+                {#each [...$output.containers[containerid]].reverse() as objectid}
+                    <li>{@html $output.items.get(objectid).display()}</li>
+                {/each}
+            </ul>
+        </div>
+    {/each}
 </div>
--- src/output.js
+++ src/output.js
@@ -1,66 +1,303 @@
 import { writable } from 'svelte/store';
 const AU = require('ansi_up');
 const ansiup = new AU.default;
+const { ipcRenderer } = require('electron');
+
+function strip_colors(str) {
+  return str.replace(/@./g, "").replace(/\u001B\[\d;\d\dm/g, "")
+}
+
+class DefaultMap extends Map {
+  constructor(getDefaultValue, ...mapConstructorArgs) {
+    super(mapConstructorArgs);
+
+    if (typeof getDefaultValue !== 'function') {
+      throw new Error('getDefaultValue must be a function');
+    }
+
+    this.getDefaultValue = getDefaultValue;
+  }
+
+  get(key) {
+    if (!this.has(key)) {
+      this.set(key, this.getDefaultValue(key));
+    }
+
+    return super.get(key);
+  };
+};
+
+class Item {
+  constructor(invitem) {
+    if (!invitem) {
+      this.flags = "";
+      this.itemname = "";
+      this.level = "";
+      this.type = "";
+      this.unique = "";
+      this.wear_loc = "";
+      this.timer = "";
+      return
+    }
+    let [
+      flags, itemname, level,
+      type, unique, wear_loc, timer,
+    ] = invitem.split(",");
+    this.flags = flags;
+    this.itemname = itemname;
+    this.level = level;
+    this.type = type;
+    this.unique = unique;
+    this.wear_loc = wear_loc;
+    this.timer = timer;
+  }
+
+  display() {
+    return `${strip_colors(this.itemname)} (${this.level})`
+  }
+}
+
+function addItems(self, invdata = "", containerid = "inventory") {
+  if (!invdata) {
+    return
+  }
+
+  let lines = invdata.split("\n");
+
+  self.containers[containerid] = new Set();
+  lines.reverse().forEach(function (row) {
+    let [objectid, properties] = row.split(/,(.+)/);
+    self.items.set(objectid, new Item(properties));
+    self.containers[containerid].add(objectid);
+  });
+}
 
 const { subscribe, set, update } = writable({
   log: [],
   map: '',
-  inventory: '',
   qt: null,
+  items: new DefaultMap(() => new Item()),
+  containers: {},
 });
 
-let mapContents = '';
-let gettingMap = false;
-let gettingInv = false;
-let inv = '';
+class Captor {
+  constructor() {
+    this.current = "";
+    this.buffer = "";
+    this.awaiting = new Set();
+    this.handlers = {};
+  }
 
-function extractMap(self, msg) {
-  // Grab map. If it exists, put it in the map box
-  const mapStart = msg.match(/(?<=<MAPSTART>)(.*)/s);
-  if (!mapStart && !gettingMap) {
-    return msg
+  extractBetweenTags(self, msg) {
+    // if the message has opening tag
+    let precontent = "";
+    let content = "";
+    let postcontent = "";
+    var containerid = "";
+    var handler;
+
+    if (this.current) {
+      // console.log(`continuing to capture ${this.current}`);
+      handler = this.handlers[this.current];
+      content = msg;
+    } else {
+      var key;
+      for ([key, handler] of Object.entries(this.handlers)) {
+        // console.log(`checking for "${handler.openTag}"`);
+        if (msg.match(handler.openTag)) {
+          // grab everything after opening tag
+          let bits = msg.split(handler.openTag);
+          if (bits.length == 3) {
+            [precontent, containerid, content] = bits;
+          } else {
+            [precontent, content] = bits;
+          }
+          containerid = containerid ? containerid : key;
+          this.current = containerid;
+          // console.log(`capturing ${this.current}`);
+          break
+        }
+      }
+    }
+    if (!this.current) {
+      return msg;
+    }
+    // console.log(`checking for "${handler.closeTag}"`);
+    let finish = false;
+    if (content.indexOf(handler.closeTag) > -1) {
+      // grab everything before closing tag
+      [content, postcontent] = content.split(handler.closeTag);
+      finish = true;
+    }
+    // save buffer
+    this.buffer = this.buffer + content;
+    if (finish) {
+      // fire callback
+      handler.callback(self, this.buffer, this.current)
+      // console.log(`got ${this.current}`);
+
+      // reset
+      this.awaiting.delete(this.current);
+      this.current = "";
+      this.buffer = "";
+    }
+    return precontent + postcontent;
   }
-  gettingMap = true;
-  mapContents += mapStart ? mapStart[0] : msg;
-  if (mapContents.indexOf('<MAPEND>') > -1) {
-    mapContents = mapContents.split('<MAPEND>')[0];
-    mapContents = ansiup.ansi_to_html(mapContents);
-    self.map = mapContents;
-    mapContents = '';
-    gettingMap = false;
+
+  askForContainer(containerid) {
+    var msg;
+    switch (containerid) {
+      case "inventory":
+        msg = "invdata";
+        break
+      case "equipment":
+        msg = "eqdata";
+        break;
+      default:
+        msg = `invdata ${containerid}`;
+    }
+    if (!this.awaiting.has(containerid)) {
+      // console.log(`awaiting ${containerid}`);
+      this.awaiting.add(containerid);
+      ipcRenderer.send("msg", msg);
+    }
   }
-  // remove from output log
-  // This will only replace maps that are in the same message
-  return msg.replace(/<MAPSTART>(.*)<MAPEND>/s, '');
 }
 
-function extractInventory(self, msg) {
-  // Grab inventory. It will likely come as multiple messages, so we need to keep track
-  const invStart = msg.match(/(?<={inventory})(.*)/s);
-  if (!invStart && !gettingInv) {
-    return msg
+function moveItem(self, objectid, from, to) {
+  // removeItem(self, objectid, from);
+  if (from in self.containers) {
+    self.containers[from].delete(objectid);
+  } else {
+    captor.askForContainer(from);
   }
-  gettingInv = true;
-  inv += invStart ? invStart[0] : msg;
-  // if the inventory has closing tag
-  if (inv.indexOf('{/inventory}') > -1) {
-    // grab everything before closing tag
-    inv = inv.split('{/inventory}')[0];
-    // display inventory
-    inv = ansiup.ansi_to_html(inv);
-    self.inventory = inv;
-    // clear inventory for next time
-    inv = '';
-    gettingInv = false;
+  // addItem(self, objectid, to);
+  if (to in self.containers) {
+    self.containers[to].add(objectid);
   }
-  // remove from output log
-  return msg.replace(/({inventory})?(.*)({\/inventory})?/s, '');
+  if(!(from in self.containers) || !(to in self.containers)) {
+    captor.askForContainer(to);
+  }
+}
+
+function removeItem(self, objectid, from) {
+  if (from in self.containers) {
+    self.containers[from].delete(objectid);
+  } else {
+    captor.askForContainer(from);
+  }
+}
+
+function addItem(self, objectid, to) {
+  if (to in self.containers) {
+    self.containers[to].add(objectid);
+  } else {
+    captor.askForContainer(to);
+  }
+}
+
+let invactions = {
+  "1": function (self, objectid, containerid, wear_loc) {
+    // Removed (was worn)
+    moveItem(self, objectid, "equipment", "inventory");
+    self.items.get(objectid).wear_loc = -1;
+  },
+  "2": function (self, objectid, containerid, wear_loc) {
+    // Worn
+    moveItem(self, objectid, "inventory", "equipment");
+    self.items.get(objectid).wear_loc = wear_loc;
+  },
+  "3": function (self, objectid, containerid, wear_loc) {
+    // Removed from inventory (given away, dropped)
+    removeItem(self, objectid, "inventory");
+  },
+  "4": function (self, objectid, containerid, wear_loc) {
+    // Added to inventory (received, picked up)
+    addItem(self, objectid, "inventory");
+  },
+  "5": function (self, objectid, containerid, wear_loc) {
+    // Taken out of container
+    moveItem(self, objectid, containerid, "inventory");
+  },
+  "6": function (self, objectid, containerid, wear_loc) {
+    // Put into container
+    moveItem(self, objectid, "inventory", containerid);
+  },
+  "7": function (self, objectid, containerid, wear_loc) {
+    // Consumed (quaffed, eaten, rotted)
+    removeItem(self, objectid, "inventory");
+  },
+  "9": function (self, objectid, containerid, wear_loc) {
+    // Put into vault
+    console.error("Vaults not yet supported.");
+  },
+  "10": function (self, objectid, containerid, wear_loc) {
+    // Removed from vault
+    console.error("Vaults not yet supported.");
+  },
+  "11": function (self, objectid, containerid, wear_loc) {
+    // Put into keyring
+    moveItem(self, objectid, "inventory", "keyring");
+  },
+  "12": function (self, objectid, containerid, wear_loc) {
+    // Get from keyring
+    moveItem(self, objectid, "keyring", "inventory");
+  },
+}
+
+function extractInvmon(self, msg) {
+  let invmon = msg.match(/(?<=\{invmon\}).*/gm);
+  if (!invmon) {
+    return msg;
+  }
+  invmon.forEach((line) => {
+    let [action, objectid, containerid, wear_loc] = line.split(",");
+    invactions[action](self, objectid, containerid, wear_loc);
+  });
+  return msg;
+}
+
+function extractInvitem(self, msg) {
+  let invitem = msg.match(/(?<=\{invitem\}).*/g);
+  if (!invitem) {
+    return msg;
+  }
+  invitem.forEach((line) => {
+    // console.log(`invitem: ${line}`);
+    let [objectid, properties] = line.split(/,(.+)/)
+    self.items.set(objectid, new Item(properties));
+    self.containers["inventory"].add(objectid);
+  });
+  return msg;
+}
+
+let captor = new Captor();
+captor.handlers["map"] = {
+  "openTag": "<MAPSTART>",
+  "closeTag": "<MAPEND>",
+  "callback": (self, content) => {
+    self.map = ansiup.ansi_to_html(content);
+  }
+}
+captor.handlers["inventory"] = {
+  "openTag": /\{invdata(?: (\w+))?\}\n/,
+  "closeTag": "\n{/invdata}",
+  "callback": (self, content, type) => {
+    addItems(self, content, type);
+  },
+}
+captor.handlers["equipment"] = {
+  "openTag": "{eqdata}\n",
+  "closeTag": "\n{/eqdata}",
+  "callback": (self, content) => {
+    addItems(self, content, "equipment");
+  },
 }
 
 function parseMessage(self, msg) {
-  // msg = extractGroup(self, msg);
-  msg = extractMap(self, msg);
-  msg = extractInventory(self, msg);
+  msg = captor.extractBetweenTags(self, msg);
+  msg = extractInvmon(self, msg);
+  msg = extractInvitem(self, msg);
 
   // "[*Daily Blessing*] [678/678hp 666/666mn 993/993mv 0qt 746tnl] >"
   // const playerStats = RegExp(/(?<prefix>\[[A-Za-z: ]*)(?<hp>[0-9]+)\/(?<totalhp>[0-9]+)hp (?<mn>[0-9]+)\/(?<totalmn>[0-9]+)mn (?<mv>[0-9]+)\/(?<totalmv>[0-9]+)mv((?<qt> [0-9]+)qt)? (?<tnl>[0-9]+)tnl.+?>/g);
@@ -88,4 +325,5 @@ export const output = {
     self.log = [...self.log, convertedMsg];
     return self;
   }),
-}
+};
+export { captor, strip_colors };
