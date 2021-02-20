0cef14b76e130f2317238b9640d07c0a11f0e992
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-02-19 22:18:40 -0500

    Simplify tab handling

--- src/Inventory.svelte
+++ src/Inventory.svelte
@@ -1,25 +1,11 @@
 <script>
     import { captor, output, strip_colors } from './output';
     const { ipcRenderer } = require('electron');
+    let openTab = "inventory";
     const handleRefresh = () => {
         Object.keys($output.containers).forEach((containerid) => {
             captor.askForContainer(containerid);
         })
-    }
-    function openTab(tabName) {
-        // show chosen panel
-        let panels = document.getElementsByClassName("panel")
-        Array.from(panels).forEach(panel => {
-            panel.style.display = "none";
-        })
-        document.getElementById(`${tabName}_panel`).style.display = "block";
-
-        // highlight chosen tab
-        let tabs = document.getElementsByClassName("tab")
-        Array.from(tabs).forEach(tab => {
-            tab.classList.remove("mm_active");
-        })
-        document.getElementById(`${tabName}_tab`).classList.add("mm_active");
     }
 </script>
 
@@ -28,7 +14,7 @@
         <ul>
             {#each [...Object.keys($output.containers)].reverse() as containerid}
                 <li class="tab" id="{containerid}_tab" >
-                    <a on:click={() => {openTab(containerid)}}>{@html containerid.match(/[a-z]/i) ? containerid : strip_colors($output.items.get(containerid).itemname)}</a>
+                    <a on:click={() => {openTab = containerid;}}>{@html containerid.match(/[a-z]/i) ? containerid : strip_colors($output.items.get(containerid).itemname)}</a>
                 </li>
             {/each}
             <!-- <li>
@@ -36,13 +22,11 @@
             </li> -->
         </ul>
     </div>
-    {#each [...Object.keys($output.containers)].reverse() as containerid}
-        <div id="{containerid}_panel" class="panel" style="display: none">
-            <ul>
-                {#each [...$output.containers[containerid]].reverse() as objectid}
-                    <li>{@html $output.items.get(objectid).display()}</li>
-                {/each}
-            </ul>
-        </div>
-    {/each}
+    {#if openTab in $output.containers}
+        <ul>
+            {#each [...$output.containers[openTab]].reverse() as objectid}
+                <li>{@html $output.items.get(objectid).display()}</li>
+            {/each}
+        </ul>
+    {/if}
 </div>
